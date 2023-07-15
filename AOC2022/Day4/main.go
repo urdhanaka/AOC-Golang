33c39abce33de2bc5d6ad12bb5a48bff77cd525b
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-07-15 10:24:43 +0700

    Day4

--- AOC2022/Day4/main.go
+++ AOC2022/Day4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+
 	"fmt"
 	"strings"
 	"util"
@@ -11,7 +12,9 @@ import (
 var input string
 
 func main() {
+	fmt.Println(parseInput())
 	fmt.Println(partOne())
+	fmt.Println(partTwo())
 }
 
 func partOne() int {
@@ -40,14 +43,38 @@ func partOne() int {
 	return res
 }
 
+func partTwo() int {
+	var res int
+
+	for index := range parseInput() {
+		start1 := util.ToInt(parseInput()[index][0])
+		end1 := util.ToInt(parseInput()[index][1])
+		start2 := util.ToInt(parseInput()[index][2])
+		end2 := util.ToInt(parseInput()[index][3])
+
+		len1 := end1 - start1
+		len2 := end2 - start2
+
+		if len1 > len2 {
+			if (start2 <= end1 && start2 >= start1) || (end2 <= end1 && end2 >= start1) {
+				res++
+			}
+		} else {
+			if (start1 <= end2 && start1 >= start2) || (end1 <= end2 && end1 >= start2) {
+				res++
+			}
+		}
+	}
+
+	return res
+}
+
 func parseInput() (ans [][]string) {
 	for _, line := range strings.Split(input, "\n") {
 		row := []string{}
 
 		for _, assignments := range strings.Split(line, ",") {
-			for _, temp := range strings.Split(assignments, "-") {
-				row = append(row, temp)
-			}
+			row = append(row, strings.Split(assignments, "-")...)
 		}
 
 		ans = append(ans, row)
